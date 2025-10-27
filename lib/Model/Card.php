2d220f7acb5162fc1c679ceeba9d9cd29cbd2e5f
Ľubomír Samotný <lubomir.samotny@smartbase.sk>
2025-10-27 18:01:53 +0100

    triv: make all fields on Card entity optional

--- lib/Model/Card.php
+++ lib/Model/Card.php
@@ -11,55 +11,55 @@
 
 class Card
 {
-    public string $maskedCardNumber;
-    public string $cardHolderName;
-    public string $type;
-    public string $issuerCountry;
-    public string $cardToken;
-    public bool $cardSavingApproved;
-    public string $expiryMonth;
-    public string $expiryYear;
+    public ?string $maskedCardNumber;
+    public ?string $cardHolderName;
+    public ?string $type;
+    public ?string $issuerCountry;
+    public ?string $cardToken;
+    public ?bool $cardSavingApproved;
+    public ?string $expiryMonth;
+    public ?string $expiryYear;
 
     public static function fromArray(array $data): self
     {
         $obj = new self();
-        $obj->maskedCardNumber = $data['maskedCardNumber'];
-        $obj->cardHolderName = $data['cardHolderName'] ?? '';
-        $obj->type = $data['type'];
-        $obj->issuerCountry = $data['issuerCountry'];
-        $obj->cardToken = $data['cardToken'];
-        $obj->cardSavingApproved = $data['cardSavingApproved'] ?? false;
-        $obj->expiryMonth = $data['expiryMonth'] ?? '';
-        $obj->expiryYear = $data['expiryYear'] ?? '';
+        $obj->maskedCardNumber = $data['maskedCardNumber'] ?? null;
+        $obj->cardHolderName = $data['cardHolderName'] ?? null;
+        $obj->type = $data['type'] ?? null;
+        $obj->issuerCountry = $data['issuerCountry'] ?? null;
+        $obj->cardToken = $data['cardToken'] ?? null;
+        $obj->cardSavingApproved = $data['cardSavingApproved'] ?? null;
+        $obj->expiryMonth = $data['expiryMonth'] ?? null;
+        $obj->expiryYear = $data['expiryYear'] ?? null;
         return $obj;
     }
 
-    public function getCardToken(): string
+    public function getCardToken(): ?string
     {
         return $this->cardToken;
     }
 
-    public function getExpiryMonth(): string
+    public function getExpiryMonth(): ?string
     {
         return $this->expiryMonth;
     }
 
-    public function getExpiryYear(): string
+    public function getExpiryYear(): ?string
     {
         return $this->expiryYear;
     }
 
-    public function getCardSavingApproved(): string
+    public function getCardSavingApproved(): ?bool
     {
         return $this->cardSavingApproved;
     }
 
-    public function getMaskedCardNumber(): string
+    public function getMaskedCardNumber(): ?string
     {
         return $this->maskedCardNumber;
     }
 
-    public function getType(): string
+    public function getType(): ?string
     {
         return $this->type;
     }
