ca8c298e8511c6971d9788f6e8418ac8ae9330ab
luboSB-47 <lubomir.samotny@smartbase.sk>
2025-12-22 12:06:16 +0100

    triv: make address fields nullable as default (#4)
    * triv: make address fields nullable as default
    
    * triv: guard address fields against empty strings

--- lib/Model/Address.php
+++ lib/Model/Address.php
@@ -10,27 +10,27 @@
 namespace Raiaccept\RaiacceptApiClient\Model;
 
 class Address {
-    public string $addressStreet1;
-    public string $addressStreet2;
-    public string $addressStreet3;
-    public string $city;
-    public string $country;
-    public string $firstName;
-    public string $lastName;
-    public string $postalCode;
-    public string $state;
+    public ?string $addressStreet1;
+    public ?string $addressStreet2;
+    public ?string $addressStreet3;
+    public ?string $city;
+    public ?string $country;
+    public ?string $firstName;
+    public ?string $lastName;
+    public ?string $postalCode;
+    public ?string $state;
 
     public static function fromArray(array $data): self {
         $instance = new self();
-        $instance->addressStreet1 = $data['addressStreet1'] ?? '';
-        $instance->addressStreet2 = $data['addressStreet2'] ?? '';
-        $instance->addressStreet3 = $data['addressStreet3'] ?? '';
-        $instance->city = $data['city'] ?? '';
-        $instance->country = $data['country'] ?? '';
-        $instance->firstName = $data['firstName'] ?? '';
-        $instance->lastName = $data['lastName'] ?? '';
-        $instance->postalCode = $data['postalCode'] ?? '';
-        $instance->state = $data['state'] ?? '';
+        $instance->addressStreet1 = ($data['addressStreet1'] ?? '') !== '' ? $data['addressStreet1'] : null;
+        $instance->addressStreet2 = ($data['addressStreet2'] ?? '') !== '' ? $data['addressStreet2'] : null;
+        $instance->addressStreet3 = ($data['addressStreet3'] ?? '') !== '' ? $data['addressStreet3'] : null;
+        $instance->city = ($data['city'] ?? '') !== '' ? $data['city'] : null;
+        $instance->country = ($data['country'] ?? '') !== '' ? $data['country'] : null;
+        $instance->firstName = ($data['firstName'] ?? '') !== '' ? $data['firstName'] : null;
+        $instance->lastName = ($data['lastName'] ?? '') !== '' ? $data['lastName'] : null;
+        $instance->postalCode = ($data['postalCode'] ?? '') !== '' ? $data['postalCode'] : null;
+        $instance->state = ($data['state'] ?? '') !== '' ? $data['state'] : null;
         return $instance;
     }
 }
